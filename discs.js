230ed8c0821656384bb4411ebaf6646a8777987b
David <Rigby335@hotmail.com>
2022-05-05 15:11:35 -0700

    test REFACTORED

--- discs.js
+++ discs.js
@@ -8,4 +8,7 @@ export const goblins = [{ name: 'McStabby', hp: 2, image: './assets/goblin.jpg' 
     { name: 'Stabby McStabbo', hp: 4, image: './assets/goblin.jpg' },
 ];
 
-export const tools = [{ name: 'Spade', dig: 7, durability: 9, image: './assets/shovel.jpg', perks: ['Easy to use', 'Versatile', 'Decent at killing zombies'] }];
+export const tools = [{ name: 'Spade', dig: 7, durability: 9, image: './assets/shovel.jpg', perks: ['Easy to use', 'Versatile', 'Decent at killing zombies'] },
+    { name: 'Mattock', dig: 6, durability: 10, image: './assets/mattock.jpg', perks: ['Medium ease to use', 'Two-headed', 'Solid at killing zombies'] },
+    { name: 'Chainsaw', dig: 0, durability: 5, image: './assets/chainsaw.jpg', perks: ['Hard to use', 'Sawing action', 'Amazing at killing zombies'] },
+];
