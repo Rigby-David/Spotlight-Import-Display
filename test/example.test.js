07233f204d4a7921910ede5b31a4faf1c9191482
David <Rigby335@hotmail.com>
2022-05-05 14:00:58 -0700

    setting up goblin test

--- test/example.test.js
+++ test/example.test.js
@@ -8,11 +8,25 @@ const test = QUnit.test;
 test('test disc render function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="disc"><h1>Wraith</h1><img src="./assets/wraith.jpg"><p>The Wraith has a speed of 11 and glide of 3</p></div>`;
+    const expected = `<div class="disc"><h1>Wraith</h1><img src="./assets/wraith.jpg"><p>The Wraith has a speed of 11 and glide of 5</p></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = renderDisc(discs[0]);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
+
+test('test goblin render function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = ``;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = ;
 
     //Expect
     // Make assertions about what is expected versus the actual result
