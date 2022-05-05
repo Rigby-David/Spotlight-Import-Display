58aeb5208514a0139a75b56192906483fc14bd60
David <Rigby335@hotmail.com>
2022-05-05 12:30:26 -0700

    finished first array

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 import { discs } from './discs.js';
 import { renderDisc } from './utils.js';
 // let state
-
+const discListEl = document.getElementById('discs');
+for (let disc of discs) {
+    const discDiv = renderDisc(disc);
+    discListEl.append(discDiv);
+}
 // set event listeners 
   // get user input
   // use user input to update state 
--- discs.js
+++ discs.js
@@ -1,1 +1,4 @@
-export const discs = [{ name: 'Wraith', speed: 11, glide: 3, image: './assets/wraith.jpg' }];
+export const discs = [{ name: 'Wraith', speed: 11, glide: 5, image: './assets/wraith.jpg' },
+    { name: 'Whale', speed: 2, glide: 3, image: './assets/whale.jpg' },
+    { name: 'Buzzz', speed: 5, glide: 4, image: './assets/buzzz.jpg' },
+];
